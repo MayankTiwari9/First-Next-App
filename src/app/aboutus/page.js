8fef88a3b61c31146b9baf049e4275e2c3bfb025
MayankTiwari9 <mt1027319@gmail.com>
2023-12-15 14:51:01 +0530

    Dynamic Route Added

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const AboutUs = () => {
+  const details = [
+    { id: 1, name: "Yash", role: "Senior Developer" },
+    { id: 2, name: "Vaibhav", role: "Backend Developer" },
+    { id: 3, name: "Suresh", role: "Frontend Developer" },
+  ];
+
   return (
     <div>
-      AboutUs
+      <h1>AboutUs</h1>
+      {details.map((detail) => {
+        return (
+          <Link href={`/aboutus/${detail.id}`}>
+            <ul key={detail.id}>
+              <li>{detail.name}</li>
+              <li>{detail.role}</li>
+            </ul>
+          </Link>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default AboutUs;
